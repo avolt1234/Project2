82cb4ca4489507b6499b9669375f942480faae3c
Alex Voultos <voultoa@gmail.com>
2018-09-24 19:44:10 -0400

    WIP

--- Project2Main.py
+++ Project2Main.py
@@ -1,11 +1,17 @@
 import datetime
 from dateutil.relativedelta import relativedelta as rd
 
+def runTheProgram():
+    breaker = True
+
 def getAge():
     today = datetime.datetime.today()
     date = input("Enter your birthday (mm/dd/YYYY): ")
-    date = datetime.datetime.strptime(date, "%m/%d/%Y")
-
+    try:
+        date = datetime.datetime.strptime(date, "%m/%d/%Y")
+    except:
+        print("Error in the date")
+        return
     age = rd(today, date).years
 
     print(str(age))
